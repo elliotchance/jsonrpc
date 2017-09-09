a21e234562cf90baed01389ef2c510b6376c7e00
Elliot Chance <elliotchance@gmail.com>
2017-09-09 19:55:27 +1000

    Only support Go 1.7+

--- jsonrpc.go
+++ jsonrpc.go
@@ -4,4 +4,9 @@
 // This package provides an implementation for a JSON-RPC server (see
 // SimpleServer) that supports all of the version 2.0 spec, including batch
 // requests.
+//
+// Installation
+//
+//     go get -u github.com/elliotchance/jsonrpc
+//
 package jsonrpc
